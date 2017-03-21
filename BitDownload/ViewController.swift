cc7df7e93c63219ecccc526b96f48bd73712cd9f
MacPower <thomas.buatois@gmail.com>
2017-03-21 10:25:54 +0100

    Initial Commit

---
+++ BitDownload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BitDownload
+//
+//  Created by Thomas Buatois on 21/03/2017.
+//  Copyright © 2017 Thomas Buatois. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
