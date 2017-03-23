2bd51dcdfe6e57e5db4accc0d697ab14c513c86a
MacPower <thomas.buatois@gmail.com>
2017-03-23 11:25:48 +0100

    Add authentification + textfield for user and password

--- BitDownload/ViewController.swift
+++ BitDownload/ViewController.swift
@@ -7,16 +7,35 @@
 //
 
 import Cocoa
+import Alamofire
 
 class ViewController: NSViewController {
 
     
     @IBOutlet weak var TitleLabelField: NSTextField!
     @IBOutlet weak var ContentSearchField: NSSearchField!
+    @IBOutlet weak var TextFieldTest: NSScrollView!
+    @IBOutlet weak var GetAuth: NSButton!
+    @IBOutlet weak var UsernameTextField: NSTextField!
     
-    @IBAction func TorrentSearchField(_ sender: Any) {
+    @IBOutlet weak var PasswordSecureTextField: NSSecureTextField!
+    @IBAction func GetAuth(_ sender: Any) {
         
+        
+        let url = "https://api.t411.li/auth"
+        
+        let parameters = [
+            "username": UsernameTextField.stringValue,
+            "password": PasswordSecureTextField.stringValue
+        ]
+
+        
+        Alamofire.request(url, method: .post, parameters: parameters)
+                .responseJSON { response in
+                debugPrint(response)
+        }
     }
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
